681bb24a761398924a965bfa47983f3f35ffdc4c
The Sharp Ninja <ninja@thesharp.ninja>
2022-02-16 12:06:41 -0600

    Hosts Correctly Now

--- Servers/Homeschool.Server/GlobalUsings.cs
+++ Servers/Homeschool.Server/GlobalUsings.cs
@@ -1,7 +1,10 @@
-﻿global using Homeschool.DomainModels.Grades;
+﻿global using Homeschool.Data.Context;
+global using Homeschool.DomainModels.Grades;
 global using Homeschool.Server;
 
 global using CoreWCF;
 global using CoreWCF.Configuration;
 
+global using Microsoft.EntityFrameworkCore;
 
+global using System.Diagnostics;
--- Servers/Homeschool.Server/Program.cs
+++ Servers/Homeschool.Server/Program.cs
@@ -1,64 +1,119 @@
 
-using Homeschool.Data.Context;
+Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
 
-using Microsoft.EntityFrameworkCore;
+//using IHost host = Host.CreateDefaultBuilder(args)
+//    .UseWindowsService(
+//        options =>
+//        {
+//            options.ServiceName = "Homeschool Service";
+//        }
+//    )
+//    .ConfigureAppConfiguration(
+//        builder =>
+//        {
+//            builder.AddJsonFile("appsettings.json", false);
+//            builder.AddUserSecrets(typeof(GradesService).Assembly, true);
+//        })
+//    .ConfigureLogging(builder => builder.AddConsole())
+//    .ConfigureServices(
+//        services =>
+//        {
+//            services
+//                .AddSingleton(
+//                    provider =>
+//                    {
+//                        ILogger<GradesService> logger  = provider.GetService<ILogger<GradesService>>()!;
+//                        HomeschoolContext context = provider.GetService<HomeschoolContext>()!;
 
-using IHost host = Host.CreateDefaultBuilder(args)
-    .UseWindowsService(
-        options =>
+//                        return new GradesService(logger, context);
+//                    })
+//                .AddSingleton(
+//                    provider =>
+//                    {
+//                        string cs
+//                            = provider.GetRequiredService<IConfiguration>()["ConnectionString"];
+
+//                        return new DbContextOptionsBuilder<HomeschoolContext>()
+//                            .EnableDetailedErrors(true)
+//                            .UseSqlServer(
+//                                cs,
+//                                builder =>
+//                                {
+//                                }
+//                            )
+//                            .Options;
+//                    }
+//                )
+//                .AddDbContext<HomeschoolContext>()
+//                .AddServiceModelServices();
+
+//            services.AddHostedService(
+//                provider =>
+//                {
+//                    GradesService gradesService = provider.GetService<GradesService>()!;
+//                    ILogger<WindowsBackgroundService> logger = provider.GetService<ILogger<WindowsBackgroundService>>()!;
+
+//                    return new WindowsBackgroundService(gradesService, logger);
+//                });
+//        }
+//    )
+//    .Build();
+
+//await host.RunAsync();
+
+
+var builder = WebApplication.CreateBuilder();
+builder.Host.UseWindowsService(options =>
         {
             options.ServiceName = "Homeschool Service";
-        }
-    )
-    .ConfigureAppConfiguration(
+        });
+builder.WebHost.ConfigureAppConfiguration(
         builder =>
         {
             builder.AddJsonFile("appsettings.json", false);
             builder.AddUserSecrets(typeof(GradesService).Assembly, true);
-        })
-    .ConfigureLogging(builder => builder.AddConsole())
-    .ConfigureServices(
-        services =>
+        });
+builder.WebHost.ConfigureKestrel(
+    static (context, options) =>
+    {
+        options.AllowSynchronousIO = true;
+        options.ListenAnyIP(5000, listenOptions =>
         {
-            services
-                .AddSingleton(
-                    provider =>
-                    {
-                        ILogger<GradesService> logger  = provider.GetService<ILogger<GradesService>>()!;
-                        HomeschoolContext context = provider.GetService<HomeschoolContext>()!;
+            if (Debugger.IsAttached)
+            {
+                listenOptions.UseConnectionLogging();
+            }
+        });
+    });
+builder.Services.AddSingleton(
+        provider =>
+        {
+            string cs = provider.GetRequiredService<IConfiguration>()["ConnectionString"];
 
-                        return new GradesService(logger, context);
-                    })
-                .AddSingleton(
-                    provider =>
-                    {
-                        string cs
-                            = provider.GetRequiredService<IConfiguration>()["ConnectionString"];
-
-                        return new DbContextOptionsBuilder<HomeschoolContext>()
-                            .EnableDetailedErrors(true)
-                            .UseSqlServer(
-                                cs,
-                                builder =>
-                                {
-                                }
-                            )
-                            .Options;
-                    }
+            return new DbContextOptionsBuilder<HomeschoolContext>()
+                .EnableDetailedErrors(true)
+                .UseSqlServer(
+                    cs,
+                    builder => { }
                 )
-                .AddDbContext<HomeschoolContext>()
-                .AddServiceModelServices();
-
-            services.AddHostedService(
-                provider =>
-                {
-                    GradesService gradesService = provider.GetService<GradesService>()!;
-                    ILogger<WindowsBackgroundService> logger = provider.GetService<ILogger<WindowsBackgroundService>>()!;
-
-                    return new WindowsBackgroundService(gradesService, logger);
-                });
+                .Options;
         }
     )
-    .Build();
+    .AddTransient<GradesService>()
+    .AddDbContext<HomeschoolContext>()
+    .AddServiceModelServices();
 
-await host.RunAsync();
+var app = builder.Build();
+
+app.UseServiceModel(
+    builder =>
+    {
+        builder.AddService<GradesService>()
+                    .AddServiceEndpoint<GradesService, IGradesService>(
+                        new BasicHttpBinding(),
+                        "/GradesService/basichttp"
+                    );
+    }
+        );
+app.Run();
+
