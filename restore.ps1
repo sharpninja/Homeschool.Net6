03a6723073fb300b78e41a55eaeed4aaabe41e85
The Sharp Ninja <ninja@thesharp.ninja>
2022-02-18 17:28:41 -0600

    ps1

--- restore.ps1
+++ restore.ps1
@@ -1,8 +1,10 @@
 Push-Location
 try {
-    dotnet restore Homeschool.Net6.sln --configfile Servers\nuget.config
-    Set-Location Servers
-    dotnet restore Servers.sln --configfile Servers\nuget.config
+    $config = (Get-Item Servers\nuget.config).FullName
+    $projects = Get-ChildItem *.csproj -Path Client -Recurse
+    $projects += Get-ChildItem *.csproj -Path Server -Recurse
+    $projects += Get-ChildItem *.csproj -Path Servers -Recurse
+    $projects.FullName | ForEach-Object -Process { Set-Location (Split-path $_); & dotnet restore "$_" --configfile "$config"}
 }
 finally {
     Pop-Location
