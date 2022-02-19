bd649d2ac7a2c1d3c8c3586a4756971793ed2385
The Sharp Ninja <ninja@thesharp.ninja>
2022-02-18 18:38:16 -0600

    PS1

--- restore.ps1
+++ restore.ps1
@@ -1,13 +1,14 @@
 Push-Location
 try {
     $config = (Get-Item Servers\nuget.config).FullName
-    $projects = Get-ChildItem *.csproj -Path Client -Recurse
-    $projects += Get-ChildItem *.csproj -Path Server -Recurse
-    $projects += Get-ChildItem *.csproj -Path Servers -Recurse
-    $projects.FullName | ForEach-Object -Process { Set-Location (Split-path $_); & dotnet restore "$_" --configfile "$config"}
+    # $projects = Get-ChildItem *.csproj -Path Client -Recurse
+    # $projects += Get-ChildItem *.csproj -Path Server -Recurse
+    # $projects += Get-ChildItem *.csproj -Path Servers -Recurse
+    # $projects.FullName | ForEach-Object -Process { Set-Location (Split-path $_); & dotnet restore "$_" --configfile "$config"}
 
-    Pop-Location
+    # Pop-Location
     Set-Location Servers
+    dotnet restore Servers.sln -c Debug --configfile "$config"
     dotnet build Servers.sln --no-restore -c Debug
     dotnet publish Servers.sln --no-build -c Debug -a x64 -o "${env:outdir}"
 }
